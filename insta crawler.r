7fd2758c0f1bfe1bc6b971acded6874b044690f0
Arin <arinv1618beta@gmail.com>
2016-07-11 16:46:17 -0500

    Instagram crawler

--- insta crawler.r
+++ insta crawler.r
@@ -1,22 +1,58 @@
-source <- GET("https://www.instagram.com/p/BHt_EZVBdC5") # normal
-source <- GET("https://www.instagram.com/p/BHSBpF0BSU-/") # video
+library("httr")
+library("jsonlite")
+library("stringr")
 
-r <- content(source, "text")
-spilit <- strsplit(r, "<script type=\"text/javascript\">window._sharedData =")
-data <- spilit[[1]][2] #get second element
-s <- strsplit(data, "</script")
-data <- s[[1]][1]
-data <- substr(data, 1, str_length(data)-1) #remove the trailing ;
-df <- fromJSON(data)
+list.of.urls <- read.csv("instagram_list.csv")
+report.df <- data.frame(numeric(), numeric(), logical() , numeric(), numeric())
 
-#details for reach
-#username <- df$entry_data$PostPage$media$owner$username
-#source <- GET("https://www.instagram.com/carinkilbyclark")
-#followed_by <- df$entry_data$ProfilePage$user$followed_by$count
+for(i in 1:nrow(list.of.urls))
+{
+	url <- list.of.urls[i,]
+	#print(url)
+	instaPostDF <- crawlMyInsta(url)
 
-comment_count <- df$entry_data$PostPage$media$comments$count
-likes_count <- df$entry_data$PostPage$media$likes$count
+	comment_count <- as.numeric(instaPostDF$entry_data$PostPage$media$comments$count)
+	likes_count <- as.numeric(instaPostDF$entry_data$PostPage$media$likes$count)
+	#print(comment_count)
 
-#video
-is_video <- df$entry_data$PostPage$media$is_video
-video_views <- df$entry_data$PostPage$media$video_views
+	video_views = 0
+	is_video <- as.logical(instaPostDF$entry_data$PostPage$media$is_video)
+	
+	if(!is_video)
+	{
+		
+	}
+	else
+	{
+		video_views <- as.numeric(df$entry_data$PostPage$media$video_views)
+	}
+
+	username <- as.character(instaPostDF$entry_data$PostPage$media$owner$username)
+
+	#create the profile URL
+	instaProfileURL <- paste("https://www.instagram.com/",username, sep="")
+	profileDF <- crawlMyInsta(instaProfileURL)
+	followed_by <- as.numeric(profileDF$entry_data$ProfilePage$user$followed_by$count)
+	#print(followed_by)
+
+	row <- c(comment_count, likes_count, is_video, video_views, followed_by)
+	print(row)
+	report.df <- rbind(report.df, row)
+}
+
+colnames(report.df) <- c("Comments", "Likes","is_video", "Video Views", "Reach")
+write.csv(report.df, file = "export-instagram.csv", row.names = F)
+
+
+crawlMyInsta <- function(url)
+{
+	source <- GET(toString(url))
+	r <- content(source, "text", encoding = "UTF-8")
+	spilit <- strsplit(r, "<script type=\"text/javascript\">window._sharedData =")
+	data <- spilit[[1]][2] #get second element
+	s <- strsplit(data, "</script")
+	data <- s[[1]][1]
+	data <- substr(data, 1, str_length(data)-1) #remove the trailing ;
+	data.frame <- fromJSON(data)
+	return (data.frame)
+}
