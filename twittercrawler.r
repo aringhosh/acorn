bfeff37e92a620ae1070b1bc566c773a21ae80ee
Arin <arinv1618beta@gmail.com>
2016-09-13 12:06:51 -0500

    added RT count feature for twitter

--- twittercrawler.r
+++ twittercrawler.r
@@ -8,16 +8,29 @@ a_sec = "Ru7pwWvE2tTEAuGRRSbk8SXcWEjCBCamQ4PrRJLJY"
 
 setup_twitter_oauth(consumer_key = c_key, consumer_secret = c_sec, access_token = a_tok, access_secret = a_sec)
 
+calculate_RT <- FALSE
+
+if(calculate_RT){
+  print("calculating RT reach") 
+} else{
+  print("not calculating RT reach")
+}
+  
+
+#helper function
+trim <- function (x) gsub("^\\s+|\\s+$", "", x)
+
 list.of.urls <- read.csv("twitter_list.csv", stringsAsFactors=FALSE)
-report.df <- data.frame(character(), numeric(), numeric(), numeric())
+report.df <- data.frame(character(), numeric(), numeric(), numeric(), numeric())
 
 for (i in 1:nrow(list.of.urls))
 {
   fav <- 0
   rt <- 0
   reach <- 0
+  rt_reach <- 0
 
-  status.url<- list.of.urls[i,1]
+  status.url<- trim(list.of.urls[i,1])
   print(paste(i, " of ", nrow(list.of.urls)))
   #print( paste("url: ", status.url))
 
@@ -30,22 +43,45 @@ for (i in 1:nrow(list.of.urls))
       rt <- st$retweetCount
       u <- getUser(st$screenName)
       reach <- u$followersCount
-      #retweeters(st$getId(), n=100)
+      
+      #RT reach count
+      if(calculate_RT)
+      {
+        rts <- retweeters(st$getId(), n=100)
+        rt_count <- length(rts)
+        
+        for(j in 1:rt_count)
+        {
+          tryCatch(
+          {
+            #print(paste(j, "of", rt_count, " - ", rts[j]))
+            userreach <- getUser(rts[j])$followersCount
+            #print(userreach)
+            rt_reach <- rt_reach + userreach
+          },warning = function(w) { 
+            #print("warning")
+          }
+          ,error = function(e) { 
+            print(paste("ERR:: ", rts[j]))
+          }
+          )
+        }
+      }
+      
     },
     warning = function(w) { 
         #print("warning")
-    }
-      ,error = function(e) { 
+    },error = function(e) { 
         print(paste("ERR:: ", status.url))
       }
     )
 
-  row <- data.frame(status.url, fav, rt, reach)
+  row <- data.frame(status.url, fav, rt, reach, rt_reach)
   report.df <- rbind(report.df, row)
-  print(paste("fav: ", fav," RT: ", rt, " reach: ", reach))
+  print(paste("fav: ", fav," RT: ", rt, " reach: ", reach, "RT Reach:", rt_reach))
 }
 
 rownames(report.df) <- NULL
-colnames(report.df) <- c("twitter", "fav", "retweets","reach")
+colnames(report.df) <- c("twitter", "fav", "retweets","reach", "rt reach")
 write.csv(report.df, file = "export-twitter.csv", row.names = F)
 print("finished exporting results to export-twitter.csv")
