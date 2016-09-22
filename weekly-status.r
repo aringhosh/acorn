90c470d84544449f9591a1fbe270cc1337d56547
Arin <arinv1618beta@gmail.com>
2016-09-21 21:10:43 -0500

    weekly report will now be emailed to client services personnel

--- weekly-status.r
+++ weekly-status.r
@@ -1,5 +1,6 @@
 library(httr)
 library(jsonlite)
+library("mailR")
 
 #helper function
 percent <- function(x, digits = 2, format = "f", ...) {
@@ -7,19 +8,21 @@ percent <- function(x, digits = 2, format = "f", ...) {
 }
 
 topics_df <- read.csv("weekly topics list.csv", stringsAsFactors = F)
-report.df <- data.frame(character(), numeric(), numeric(), numeric(), numeric(), numeric(), numeric(), numeric(), numeric(), stringsAsFactors = F)
+report.df <- data.frame(character(), character(), character(), numeric(), numeric(), numeric(), numeric(), numeric(), numeric(), numeric(), numeric(), stringsAsFactors = F)
 
 
+#part 1: gather numbers
 base_url <- "https://api.datarank.com/topics/"
 
 for (i in 1:nrow(topics_df))
 {
   slug <- topics_df[i,1]
   start <- topics_df[i,2]
-  end <- topics_df[i,3]
+  end <- Sys.Date() #"2016-09-21" #topics_df[i,3]
   
   print(paste("processing :", slug))
   
+  #print("begining twitter")
   Datasource <- "T"
   param_url <- paste("/reach/weekly?datasource=",Datasource,"&start_date=",start,"&end_date=",end, sep = "")
   callback_url <- paste(base_url,slug,param_url, sep = "")
@@ -28,9 +31,27 @@ for (i in 1:nrow(topics_df))
   parsedDataframe <- fromJSON(jsontext)
   data <- parsedDataframe$reach$data
   T_total <- parsedDataframe$last90
-  T_last_week <- data[nrow(data),2]
-  T_last_week_per <- percent(T_last_week/T_total)
+  no_of_weeks <- length(data)
+  if(no_of_weeks == 0)
+  {
+    T_last_week <- T_last_week_per <- 0
+  }
+  else
+  {
+    T_last_week <- data[[no_of_weeks]]$data
+
+    if(T_last_week != T_total)
+    {
+      T_last_week_per <- percent(T_last_week/T_total)  
+    }
+    else
+    {
+      T_last_week_per <- percent(100)
+    }
+    
+  }
   
+  #print("begining INSTAGRAM")
   Datasource <- "INSTAGRAM"
   param_url <- paste("/reach/weekly?datasource=",Datasource,"&start_date=",start,"&end_date=",end, sep = "")
   callback_url <- paste(base_url,slug,param_url, sep = "")
@@ -39,15 +60,67 @@ for (i in 1:nrow(topics_df))
   parsedDataframe <- fromJSON(jsontext)
   data <- parsedDataframe$reach$data
   I_total <- parsedDataframe$last90
-  I_last_week <- data[nrow(data),2]
-  I_last_week_per <- percent(I_last_week/I_total)
+  no_of_weeks <- length(data)
+  if(no_of_weeks == 0)
+  {
+    I_last_week <- I_last_week_per <- 0
+  }
+  else
+  {
+    I_last_week <- data[[no_of_weeks]]$data
+
+    if(I_last_week != I_total)
+    {
+      I_last_week_per <- percent(I_last_week/I_total)  
+    }
+    else
+    {
+      I_last_week_per <- percent(100)
+    }
+  }
   
   total_reach <- T_total + I_total
   total_reach_last_week <- T_last_week + I_last_week
   
-  row <- data.frame(slug, T_total, T_last_week, T_last_week_per, I_total, I_last_week, I_last_week_per, total_reach, total_reach_last_week)
+  row <- data.frame(slug, start, end, T_total, T_last_week, T_last_week_per, I_total, I_last_week, I_last_week_per, total_reach, total_reach_last_week)
   report.df <- rbind( report.df, row)
 }
 
-write.csv(report.df, file = "export-weekly-topic-reach.csv", row.names = F)
+filename <- paste(end, "-weekly-reach-export.csv", sep = "")
+write.csv(report.df, file = filename, row.names = F)
 
+print(report.df)
+print(paste("exported to", filename))
+View(report.df)
+
+# part 2: email report
+print("send email now? y/n")
+readOption <- function(){
+  n <- readline()
+  switch(n,
+    'y' = {sendMail(filename)},
+    'n' = {print("email not sent!")},
+    {
+      print("use y or n")
+      readOption()
+    }
+          )
+}
+
+sendMail <- function(filename)
+{
+  #https://github.com/rpremraj/mailR
+  toEmail <- c("kduke@acorninfluence.com", "hhairston@acorninfluence.com", "kvaldez@acorninfluence.com",
+    "sfree@acorninfluence.com", "mhumble@acorninfluence.com")
+
+  send.mail(from = "report@acorninfluence.com",
+          to = c(""),
+          cc = c("aghosh@acorninfluence.com"),
+          subject = paste("Campaign reach for the week of", Sys.Date()),
+          body = "Weekly Report is attached. This is an automated email, please do not reply. Contact aghosh@acorninfluence.com for further assistance.",
+          smtp = list(host.name = "aspmx.l.google.com", port = 25),
+          authenticate = FALSE,
+          send = TRUE,
+          debug = TRUE,
+          attach.files = filename)
+}
