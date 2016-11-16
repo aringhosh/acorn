054019117f6ee5d1713219a366d6f8d8d460d18b
Arin <arinv1618beta@gmail.com>
2016-11-16 10:40:13 -0600

    a simple script to get follower counts

--- twitterReach.r
+++ twitterReach.r
@@ -1,5 +1,4 @@
 library("twitteR")
-library("stringr")
 
 c_key = "aW7HdzD7T8bddh6XZQsF9fV9S"
 c_sec = "DW6tly9zOZz1SWdnHF0HmGi7CMwZQf6bbWxFNKH02ge8apjKtC"
@@ -8,16 +7,13 @@ a_sec = "Ru7pwWvE2tTEAuGRRSbk8SXcWEjCBCamQ4PrRJLJY"
 
 setup_twitter_oauth(consumer_key = c_key, consumer_secret = c_sec, access_token = a_tok, access_secret = a_sec)
 
-list.of.urls <- read.csv("twitter_list.csv", stringsAsFactors=FALSE)
+list.of.urls <- read.csv("twitter_reach.csv", stringsAsFactors=FALSE)
 report.df <- data.frame(character(), numeric())
 
 for (i in 1:nrow(list.of.urls))
 {
-  fav <- 0
-  rt <- 0
   reach <- 0
-
-  screenName<- list.of.urls[i,1]
+  screenName <- list.of.urls[i,1]
   print(paste(i, " of ", nrow(list.of.urls)))
   #print( paste("url: ", status.url))
 
@@ -31,16 +27,14 @@ for (i in 1:nrow(list.of.urls))
         #print("warning")
     }
       ,error = function(e) { 
-        print(paste("ERR:: ", status.url))
+        print(paste("ERR:: ", screenName))
       }
     )
 
-  row <- data.frame(status.url , reach)
+  row <- data.frame(screenName , reach)
   report.df <- rbind(report.df, row)
   print(paste(" reach: ", reach))
 }
-
-rownames(report.df) <- NULL
 colnames(report.df) <- c("screenName","reach")
 write.csv(report.df, file = "export-twitter.csv", row.names = F)
 print("finished exporting results to export-twitter.csv")
