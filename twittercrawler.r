1aaae36de6a50c6be68a101720b7673a9d802ad1
Arin <arinv1618beta@gmail.com>
2016-11-28 13:22:33 -0600

    added RT count using native API call
    version 1

--- twittercrawler.r
+++ twittercrawler.r
@@ -1,14 +1,33 @@
 library("twitteR")
 library("stringr")
+library("jsonlite")
 
 c_key = "aW7HdzD7T8bddh6XZQsF9fV9S"
 c_sec = "DW6tly9zOZz1SWdnHF0HmGi7CMwZQf6bbWxFNKH02ge8apjKtC"
 a_tok = "9478372-uNoENCNbAcU6x82gji20sOE9orH30ZUfy7e7vKt93c"
 a_sec = "Ru7pwWvE2tTEAuGRRSbk8SXcWEjCBCamQ4PrRJLJY"
 
+#setup for TwitteR package
 setup_twitter_oauth(consumer_key = c_key, consumer_secret = c_sec, access_token = a_tok, access_secret = a_sec)
 
-calculate_RT = FALSE
+# setup for non TwitteR API call here
+#Use basic auth
+secret <- jsonlite::base64_enc(paste(c_key, c_sec, sep = ":"))
+req <- httr::POST("https://api.twitter.com/oauth2/token",
+  httr::add_headers(
+    "Authorization" = paste("Basic", gsub("\n", "", secret)),
+    "Content-Type" = "application/x-www-form-urlencoded;charset=UTF-8"
+  ),
+  body = "grant_type=client_credentials"
+);
+
+#Extract the access token
+httr::stop_for_status(req, "authenticate with twitter")
+token <- paste("Bearer", httr::content(req)$access_token)
+
+
+
+calculate_RT = TRUE
 
 if(calculate_RT){
   print("calculating RT reach") 
@@ -16,35 +35,17 @@ if(calculate_RT){
   print("not calculating RT reach")
 }
 
+rt.df <- data.frame("id"=character())
 
-getRTreach <- function(st)
+getRTreach <- function(id)
 {
   rt_reach <- 0
-
-  rts <- retweeters(st$getId(), n=100)
-  print(rts)
-  rt_count <- length(rts)
-  print(sprintf("%d RTs", rt_count))
-        
-        for(j in 1:rt_count)
-        {
-          tryCatch(
-          {
-            print(paste("RT:: ",j, "of", rt_count))
-            userreach <- getUser(rts[j])$followersCount
-            #print(sprintf(" reach %d", userreach))
-            rt_reach <- rt_reach + userreach
-          },warning = function(w) { 
-            #print("warning")
-          }
-          ,error = function(e) { 
-            print(paste("ERR:: ", rts[j]))
-            #print(rts)
-          }
-          )
-        }
-
-  print(sprintf("total RT reach : %d", rt_reach))
+  url<- sprintf("https://api.twitter.com/1.1/statuses/retweets/%s.json?count=100", id)
+  req <- httr::GET(url, httr::add_headers(Authorization = token))
+  json <- httr::content(req, as = "text")
+  results <- fromJSON(json)
+  rt_reach <- sum(results$user$followers_count)
+  #print(sprintf("total RT reach : %d", rt_reach))
   return (rt_reach)
 }
 
@@ -78,14 +79,14 @@ for (i in 1:nrow(list.of.urls))
       #RT reach count
       if(calculate_RT)
       {
-        reach2 <- getRTreach(st)
+        reach2 <- getRTreach(status_id)
       }
       
     },
     warning = function(w) { 
         #print("warning")
     },error = function(e) { 
-        print(paste("ERR:: ", status.url))
+        print(paste("ERR:: ", e, status.url))
       }
     )
 
